85aebc3e79f1544573de867947a0f0c62badc445
blackwolf-git <blackwolfteam.x@gmail.com>
2025-05-16 05:48:30 +0300

    Create SoloWolfDatabase.kt

---
+++ app/src/main/java/com/blackwolf/solowolf/data/local/SoloWolfDatabase.kt
@@ -0,0 +1,39 @@
+package com.blackwolf.solowolf.data.local
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.blackwolf.solowolf.models.User
+import com.blackwolf.solowolf.models.Workout
+import com.blackwolf.solowolf.models.WomanProfile
+
+@Database(
+    entities = [User::class, Workout::class, WomanProfile::class],
+    version = 1,
+    exportSchema = false
+)
+abstract class SoloWolfDatabase : RoomDatabase() {
+    abstract fun userDao(): UserDao
+    abstract fun workoutDao(): WorkoutDao
+    abstract fun womanProfileDao(): WomanProfileDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: SoloWolfDatabase? = null
+
+        fun getDatabase(context: Context): SoloWolfDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    SoloWolfDatabase::class.java,
+                    "solowolf_database"
+                )
+                    .fallbackToDestructiveMigration()
+                    .build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
