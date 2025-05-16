9a17314d5551b7e125ae338937a67bcf78bad2bd
blackwolf-git <blackwolfteam.x@gmail.com>
2025-05-16 04:45:35 +0300

    Create App.kt

---
+++ app/src/main/java/com/blackwolf/solowolf/App.kt
@@ -0,0 +1,16 @@
+package com.blackwolf.solowolf
+
+import android.app.Application
+import com.blackwolf.solowolf.di.appModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidContext(this@App)
+            modules(appModule)
+        }
+    }
+}
