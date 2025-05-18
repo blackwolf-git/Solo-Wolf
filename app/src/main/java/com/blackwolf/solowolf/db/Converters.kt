17057334e5d8a6eefdf0d2c6bf34fade290794a7
blackwolf-git <blackwolfteam.x@gmail.com>
2025-05-18 09:59:45 +0300

    Update Converters.kt

--- app/src/main/java/com/blackwolf/solowolf/db/Converters.kt
+++ app/src/main/java/com/blackwolf/solowolf/db/Converters.kt
@@ -1,25 +1,14 @@
+package com.blackwolf.solowolf.db
+
 import androidx.room.TypeConverter
+import com.blackwolf.solowolf.models.MuscleGroup
+import com.blackwolf.solowolf.models.Sport
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import java.util.Date
-import com.blackwolf.solowolf.models.Sport
-import com.blackwolf.solowolf.models.MuscleGroup
 
 class Converters {
     private val gson = Gson()
-
-    // For List<String>
-    @TypeConverter
-    fun fromStringList(value: String?): List<String>? {
-        if (value == null) return null
-        val listType = object : TypeToken<List<String>>() {}.type
-        return gson.fromJson(value, listType)
-    }
-
-    @TypeConverter
-    fun toStringList(list: List<String>?): String? {
-        return gson.toJson(list)
-    }
 
     // For Date
     @TypeConverter
@@ -32,29 +21,42 @@ class Converters {
         return date?.time
     }
 
+    // For List<String>
+    @TypeConverter
+    fun fromString(value: String?): List<String>? {
+        if (value == null) return null
+        val listType = object : TypeToken<List<String>>() {}.type
+        return gson.fromJson(value, listType)
+    }
+
+    @TypeConverter
+    fun toString(list: List<String>?): String? {
+        return gson.toJson(list)
+    }
+
     // For List<Sport>
     @TypeConverter
-    fun fromSportList(value: String?): List<Sport>? {
+    fun fromSports(value: String?): List<Sport>? {
         if (value == null) return null
         val listType = object : TypeToken<List<Sport>>() {}.type
         return gson.fromJson(value, listType)
     }
 
     @TypeConverter
-    fun sportListToString(list: List<Sport>?): String? {
+    fun sportsToString(list: List<Sport>?): String? {
         return gson.toJson(list)
     }
 
     // For List<MuscleGroup>
     @TypeConverter
-    fun fromMuscleGroupList(value: String?): List<MuscleGroup>? {
+    fun fromMuscleGroups(value: String?): List<MuscleGroup>? {
         if (value == null) return null
         val listType = object : TypeToken<List<MuscleGroup>>() {}.type
         return gson.fromJson(value, listType)
     }
 
     @TypeConverter
-    fun muscleGroupListToString(list: List<MuscleGroup>?): String? {
+    fun muscleGroupsToString(list: List<MuscleGroup>?): String? {
         return gson.toJson(list)
     }
 }
